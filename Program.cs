457568ebf379e0c752c321302aa6c031f6a463c3
Kirill <shadow986@mail.ru>
2023-03-14 00:03:09 +0300

    Задание 68

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿//Найти функцию Аккемана для чисел M и N с помощью рекурсии
+
+uint Akker (uint m, uint n)
+{
+    if (m == 0) 
+    {
+        return n+1;
+    }
+    else
+    {
+        if ((m != 0) && (n == 0))
+        {
+            return Akker(m-1,1);
+        }
+        else
+        {
+            return Akker(m-1, Akker(m,n-1));
+        }
+    }
+}
+
+Console.WriteLine("Введите целое положительное число M:");
+uint numberm = Convert.ToUInt32(Console.ReadLine());
+Console.WriteLine("Введите целое положительное число N:");
+uint numbern = Convert.ToUInt32 (Console.ReadLine());
+Console.WriteLine(Akker (numberm,numbern));
